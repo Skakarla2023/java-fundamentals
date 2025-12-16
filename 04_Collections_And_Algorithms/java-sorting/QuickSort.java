9e24acfce7ece2f39cc7719f55dd1341ae495eff
Skakarla2023 <skakarla2023@gmail.com>
2025-12-16 18:52:31 +0530

    Update QuickSort.java

--- 04_Collections_And_Algorithms/java-sorting/QuickSort.java
+++ 04_Collections_And_Algorithms/java-sorting/QuickSort.java
@@ -1,1 +1,42 @@
 
+package javaprograms;
+
+import java.util.*;
+
+public class QuickSort {
+
+	public static int partition(int[] nums, int left, int right) {
+		int pivot = nums[right];
+		int i = left - 1;
+		for (int j = left; j < right; j++) {
+			if (nums[j] < pivot) {
+				i++;
+				int temp = nums[i];
+				nums[i] = nums[j];
+				nums[j] = temp;
+			}
+		}
+		i++;
+		int temp = nums[i];
+		nums[i] = pivot;
+		nums[right] = temp;
+		return i;
+	}
+
+	public static void quickSort(int[] nums, int left, int right) {
+		if (left < right) {
+			int idx = partition(nums, left, right);
+			quickSort(nums, left, idx - 1);
+			quickSort(nums, idx + 1, right);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 6, 3, 9, 5, 2, 8 };
+		int n = nums.length;
+		System.out.println("Unsorted array: " + Arrays.toString(nums));
+
+		quickSort(nums, 0, n - 1);
+		System.out.println("Sorted arrays: " + Arrays.toString(nums));
+	}
+}
